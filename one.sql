13098a8e3eeb0965bfd7764a75a0acec1cb3e989
anupnagareddy <57056727+anupnagareddy@users.noreply.github.com>
2020-05-19 13:23:37 +0530

    Add files via upload

---
+++ one.sql
@@ -0,0 +1,11 @@
+\i /home/t3mp0/Privacy-preserve/Database/  create_file.sql
+set datestyle to 'DMY'; 
+\i /home/t3mp0/Privacy-preserve/Database/ credit_card.sql
+\i /home/t3mp0/Privacy-preserve/Database/ size_x.sql
+\i /home/t3mp0/Privacy-preserve/Database/ passengers.sql
+\i /home/t3mp0/Privacy-preserve/Database/ car.sql
+\i /home/t3mp0/Privacy-preserve/Database/ drivers.sql 
+set datestyle to 'MDY'; 
+\i /home/t3mp0/Privacy-preserve/Database/ complete_ride.sql
+\i /home/t3mp0/Privacy-preserve/Database/ requests.sql
+\i /home/t3mp0/Privacy-preserve/Database/ curr_ride.sql
